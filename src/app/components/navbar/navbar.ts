10d9d4d630437900c278acb15bc338c05906b754
LucaSoftwareEngineer <santoro.luca.ing@gmail.com>
2026-06-27 15:38:31 +0200

    fix: remove duplicate RouterLink import

--- src/app/components/navbar/navbar.ts
+++ src/app/components/navbar/navbar.ts
@@ -3,7 +3,7 @@ import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-navbar',
-  imports: [RouterLink, RouterLink],
+  imports: [RouterLink],
   templateUrl: './navbar.html',
   styleUrl: './navbar.css',
 })
